d2a786fdcb4745d35c51fffbf95b0659e10b0096
Rene Derum <renederum@gmail.com>
2021-11-05 19:39:33 -0400

    Segunda prueba de envio de email

--- enviar.php
+++ enviar.php
@@ -1,15 +1,17 @@
 <?php
-$correo = $_POST["email"];
-$telefono = $_POST["phone"];
-$mensaje = $_POST["message"]
+    $correo = $_POST["email"];
+    $telefono = $_POST["phone"];
+    $mensaje = $_POST["message"];
 
-$destinatario = "renederum@gmail.com"
-$asunto = "New Contact from WEB"
+    $destinatario = "renederum@gmail.com";
+    $asunto = "New Contact from WEB";
 
-$carta = "Correo: $correo \n";
-$carta .= "Telefono: $telefono \n";
-$carta .= "Mensaje: $mensaje \n";
+    $carta = "Correo: $correo \n";
+    $carta .= "Telefono: $telefono \n";
+    $carta .= "Mensaje: $mensaje \n";
 
-mail($destinatario, $asunto, $carta);
-header('Location:confirmacion.html');
+    $mail = mail($destinatario, $asunto, $carta);
+    if($mail){
+        echo "<h4>Enviado exitosamente</h4>";
+    }
 ?>
